d549152aceb271f447c7218ff538db7f6b1bcd01
Ha Quanq <quangha0120010@gmail.com>
2025-10-16 06:45:27 +0700

    Change purple theme icon, icons display order

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import MoonIcon from "../assets/images/moon-star-icon.svg?component";
-import SparkleIcon from "../assets/images/sparkles-icon.svg?component";
+import SkullIcon from "../assets/images/skull-icon.svg?component";
 import SunIcon from "../assets/images/sun-icon.svg?component";
 import { THEMES, useThemeStore } from "../stores/themeStore";
 
@@ -16,9 +16,9 @@ const themeStore = useThemeStore();
       @click="themeStore.cycleThemes()"
     >
       <span class="sr-only" aria-live="polite">{{ `Change theme (current: ${themeStore.theme})` }}</span>
-      <SunIcon :class="themeStore.theme != THEMES.DARK && 'hidden'" />
-      <SparkleIcon :class="themeStore.theme != THEMES.LIGHT && 'hidden'" />
-      <MoonIcon :class="themeStore.theme != THEMES.PURPLE && 'hidden'" />
+      <SunIcon :class="themeStore.theme != THEMES.LIGHT && 'hidden'" />
+      <SkullIcon :class="themeStore.theme != THEMES.PURPLE && 'hidden'" />
+      <MoonIcon :class="themeStore.theme != THEMES.DARK && 'hidden'" />
       <span class="bg-button-secondary-background absolute inset-0 rounded-[inherit] opacity-0 blur-sm"></span>
     </button>
   </header>
