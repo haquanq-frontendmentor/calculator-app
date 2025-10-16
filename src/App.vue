2a2a9fa88b4657bdb545287e36c0d8597131e95a
Ha Quanq <quangha0120010@gmail.com>
2025-10-16 19:05:23 +0700

    Add Header with theme button, background images

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import bgDark from "./assets/images/bg-dark.jpg";
+import bgLight from "./assets/images/bg-light.jpg";
+import bgPurple from "./assets/images/bg-purple.jpg";
+import Header from "./components/Header.vue";
+import { useThemeStore } from "./stores/themeStore";
 
-<template></template>
+const themeStore = useThemeStore();
+</script>
+
+<template>
+  <div class="bg-background relative flex min-h-screen items-center justify-center">
+    <div class="absolute inset-0 *:h-full *:w-full *:object-cover *:transition-opacity">
+      <img
+        :src="bgPurple"
+        alt=""
+        class="absolute inset-0 opacity-0"
+        :class="themeStore.theme == 'purple' && 'opacity-100'"
+      />
+      <img
+        :src="bgLight"
+        alt=""
+        class="absolute inset-0 opacity-0"
+        :class="themeStore.theme == 'light' && 'opacity-100'"
+      />
+      <img
+        :src="bgDark"
+        alt=""
+        class="absolute inset-0 opacity-0"
+        :class="themeStore.theme == 'dark' && 'opacity-100'"
+      />
+    </div>
+    <div class="relative z-50 w-[min(100vw-3rem,33.75rem)]">
+      <Header />
+    </div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import MoonIcon from "../assets/images/moon-star-icon.svg?component";
+import SparkleIcon from "../assets/images/sparkles-icon.svg?component";
+import SunIcon from "../assets/images/sun-icon.svg?component";
+import { THEMES, useThemeStore } from "../stores/themeStore";
+
+const themeStore = useThemeStore();
+</script>
+
+<template>
+  <header class="text-foreground flex items-center justify-between pt-2 pb-7">
+    <div class="pl-[0.375rem] text-[2rem]" aria-hidden="true">calc</div>
+    <button
+      class="text-foreground relative flex aspect-square w-10 items-center justify-center overflow-hidden rounded-lg transition-[scale] active:scale-[0.97]"
+      type="button"
+      @click="themeStore.cycleThemes()"
+    >
+      <span class="sr-only" aria-live="polite">{{ `Change theme (current: ${themeStore.theme})` }}</span>
+      <SunIcon :class="themeStore.theme != THEMES.DARK && 'hidden'" />
+      <SparkleIcon :class="themeStore.theme != THEMES.LIGHT && 'hidden'" />
+      <MoonIcon :class="themeStore.theme != THEMES.PURPLE && 'hidden'" />
+      <span class="bg-button-secondary-background absolute inset-0 rounded-[inherit] opacity-0 blur-sm"></span>
+    </button>
+  </header>
+</template>
